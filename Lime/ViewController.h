c4a151cbdbf24b1a0792696f86d0514b8bf582f0
Pavel Zarudnev <pavel@MacBook-Air.local>
2015-04-10 10:59:13 +0300

    Initial Commit

---
+++ Lime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lime
+//
+//  Created by Pavel on 10.04.15.
+//  Copyright (c) 2015 Zitech Mobile LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Lime/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Lime
+//
+//  Created by Pavel on 10.04.15.
+//  Copyright (c) 2015 Zitech Mobile LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
